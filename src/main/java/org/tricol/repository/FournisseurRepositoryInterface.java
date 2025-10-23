8068e23ec7201af0425ddd24a96ab7854c17259a
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-10-23 13:57:00 +0100

    add custom query for email domain search

--- src/main/java/org/tricol/repository/FournisseurRepositoryInterface.java
+++ src/main/java/org/tricol/repository/FournisseurRepositoryInterface.java
@@ -1,8 +1,14 @@
 package org.tricol.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.tricol.model.Fournisseur;
 
+import java.util.List;
 
 public interface FournisseurRepositoryInterface extends JpaRepository<Fournisseur, Long> {
+    List<Fournisseur> findByNom(String nom);
+
+    @Query("SELECT f FROM Fournisseur f WHERE f.email ILIKE %?1")
+    List<Fournisseur> findByEmailEndingWith(String ending);
 }
